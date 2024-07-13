91886929669bca2f3aadbebd99cf87a1fa183dd2
Dimitar32 <107855947+Dimitar32@users.noreply.github.com>
2024-07-13 09:55:15 +0300

    Add files via upload

---
+++ 06. Exercise JS Basic Syntax, Conditional Statements, and Loops/MultiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        let result = num * i;
+        console.log(num + " X " + i + " = " + result);
+    }
+}
+
+multiplTable(5);
